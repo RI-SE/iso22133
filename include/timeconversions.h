1263e3bc36194babd8d8c16734bfeb4e7308ce2c
Lukas Wikander <lukas.wikander@astazero.com>
2022-11-29 23:30:20 +0100

    Moved time conversions into own file

---
+++ include/timeconversions.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <stdint.h>
+#include <sys/time.h>
+
+// Time functions
+int8_t setToGPStime(struct timeval *time, const uint16_t GPSweek, const uint32_t GPSqmsOfWeek);
+int32_t getAsGPSWeek(const struct timeval *time);
+int64_t getAsGPSQuarterMillisecondOfWeek(const struct timeval *time);
+uint64_t getAsGPSms(const struct timeval *time);
