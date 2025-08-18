09ed4d51ad8a1a2002a4e8ce6b73a4f7a2eb4eee
Robert Brenick <robert.brenick@astazero.com>
2025-08-18 12:47:28 +0200

    Update ISO22133

--- src/osem.c
+++ src/osem.c
@@ -396,7 +396,7 @@ ssize_t decodeOSEMMessage(
 		return retval;
 	}
 
-	if (debug) {
+	if (true) {
 		printf
 			("OSEM message:\n\tID struct value ID: 0x%x\n\tID struct content length: %u"
 			 "\n\tDevice ID: %u \n\tSub device ID: %u \n\tSystem control centre ID: %u"
